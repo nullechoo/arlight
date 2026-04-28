6ad0ae2778f972237480aaaf8241e250f552b684
nullechoo <n4mesurn4me@yandex.ru>
2026-04-28 21:08:06 +0300

    fixed shared workers

--- src/shared-worker.js
+++ src/shared-worker.js
@@ -3,9 +3,14 @@ import { getCurrentTheme, setCallback, setCurrentTheme } from "/src/theme.js";
 const worker = new SharedWorker(`/src/worker.js`);
 const tabsCounter = document.querySelector("#tabs-counter");
 setCallback(() => worker.port.postMessage(getCurrentTheme()));
+
 worker.port.onmessage = (event) => {
   tabsCounter.textContent = event.data.tabs;
   setCurrentTheme(getCurrentTheme());
 };
 
 worker.port.postMessage(getCurrentTheme());
+
+window.addEventListener("beforeunload", () => {
+  worker.port.postMessage("close");
+});
--- src/worker.js
+++ src/worker.js
@@ -1,20 +1,45 @@
-const ports = [];
+let ports = new Map();
 
 self.onconnect = (initEvent) => {
   const port = initEvent.source;
+  const portId = Date.now() + "_" + Math.random();
 
-  ports.push(port);
+  ports.set(portId, port);
 
-  port.onmessage = (event) => {
-    ports.forEach((p) =>
-      p.postMessage({
-        tabs: ports.length,
-        theme: event.data,
-      }),
-    );
+  port.onmessage = (e) => {
+    ports.forEach((p) => {
+      if (e.data === "close") {
+        ports.delete(portId);
+      }
+
+      broadcastCount(p);
+    });
+  };
+
+  port.onclose = (e) => {
+    ports.delete(portId);
+    broadcastCount(e);
   };
 };
 
-self.onclose = (e) => {
-  ports.filter((item) => item !== e.source);
-};
+function broadcastCount(e) {
+  ports.forEach((p, id) => {
+    try {
+      p.postMessage({
+        tabs: ports.size,
+        theme: e.data,
+      });
+    } catch (e) {
+      ports.delete(id);
+    }
+  });
+}
+
+setInterval(() => {
+  ports.forEach((port, id) => {
+    try {
+    } catch (e) {
+      ports.delete(id);
+    }
+  });
+}, 5000);
